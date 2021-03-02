0ed3fbdb1dd409e2a06319779a931a1d7222730e
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-03-02 12:54:36 +0100

    Aggiunto controllo

--- snack-5/js/script.js
+++ snack-5/js/script.js
@@ -2,7 +2,13 @@ var numContainer = [];
 
 for ( var i = 0; i < 6; i++ ) {
   userNum = parseInt( prompt("Inserisci un numero intero") );
-  if ( userNum % 2 == 1 ) {
+  if ( isNaN( userNum ) ) {
+    risposta = confirm("Il valore non è valido, vuoi reinserire il numero? altrimenti non verrà assegnato un valore")
+    if ( risposta == true ) {
+      userNum = parseInt( prompt("Reinserisci il numero intero") );
+    }
+  }
+   else if ( userNum % 2 == 1 ) {
     numContainer[i] = userNum;
   }
 }
