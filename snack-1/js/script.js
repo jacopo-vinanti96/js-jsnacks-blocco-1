3e4e17c7a538363e0cad7ee97bf75ea3247624ed
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-03-02 11:17:53 +0100

    Algoritmo confronto numeri completato

---
+++ snack-1/js/script.js
@@ -0,0 +1,15 @@
+var num1 = parseInt(prompt("Inserisci un numero")),
+    num2 = parseInt(prompt("Inserisci un altro numero"));
+console.log(num1, num2)
+
+if ( isNaN( num1 ) || isNaN( num2 ) ) {
+  alert("Ricarica la pagina, l' algoritmo funziona solo con numeri interi")
+} else {
+  if ( num1 > num2 ) {
+    alert("il primo numero inserito è maggiore");
+  } else if ( num1 < num2 ) {
+    alert("il secondo numero inserito è maggiore");
+  } else {
+    alert("I numeri inseriti sono pari");
+  }
+}
