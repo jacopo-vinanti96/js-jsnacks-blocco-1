dc8835435727e3d1eb1193310dbf05bbfe5a2272
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-03-02 12:03:36 +0100

    Aggiunto controllo

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -4,7 +4,9 @@ for ( var i = 0; i < 10; i++ ) {
   numeroUser = parseInt( prompt("inserisci un numero") );
   numeroSomma += numeroUser;
   if ( isNaN( numeroSomma ) ) {
-    alert("Il valore inserito non è un numero intero");
+    alert("Il valore inserito non è un numero intero, riprova dall' inizio");
+    i = 0;
+    numeroSomma = 0;
   }
 }
 
