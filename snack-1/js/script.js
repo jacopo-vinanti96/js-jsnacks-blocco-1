41ca164953137b542b2303578cf6f34c55af0a27
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-03-02 11:20:18 +0100

    Fix

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,6 +1,5 @@
-var num1 = parseInt(prompt("Inserisci un numero")),
-    num2 = parseInt(prompt("Inserisci un altro numero"));
-console.log(num1, num2)
+var num1 = parseInt(prompt("Inserisci un numero intero")),
+    num2 = parseInt(prompt("Inserisci un altro numero intero"));
 
 if ( isNaN( num1 ) || isNaN( num2 ) ) {
   alert("Ricarica la pagina, l' algoritmo funziona solo con numeri interi")
