5efbdef56561267017de6206145118bc184f55cf
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-03-03 12:49:09 +0100

    Aggiunte soluzioni

--- snack-7/js/script.js
+++ snack-7/js/script.js
@@ -5,9 +5,20 @@ var esponente = 0,
 
 do {
   risultato = Math.pow( 2, esponente );
-  if ( risultato > 1000 ) {
-    break
+  if ( risultato < 1000) {
+    console.log( risultato );
+    esponente++;
   }
-  console.log( risultato );
-  esponente++;
 } while ( risultato < 1000 )
+
+// for ( var n = 1; n < 1000; n = n * 2 ) {
+//   console.log(n)
+// }
+
+// do {
+//   risultato = Math.pow( 2, esponente );
+//   if ( risultato < 1000) {
+//     console.log( risultato );
+//     esponente++;
+//   }
+// } while ( Math.pow( 2, esponente ) < 1000 )
