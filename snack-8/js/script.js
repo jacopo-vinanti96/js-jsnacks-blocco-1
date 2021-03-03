29c0260bb2ea955498969a92db9ada2f807a0b35
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-03-03 13:26:57 +0100

    Soluzione

--- snack-8/js/script.js
+++ snack-8/js/script.js
@@ -2,9 +2,9 @@
 var n = 0, risultato = 0;
 
 do {
-  n = parseInt( prompt ("Inserisci un numero di 4 cifre") )
+  n = parseInt( prompt ("Inserisci un numero di 4 cifre") );
   if ( n.toString().length != 4 ) {
-    alert("Il valore inserito non è valido")
+    alert("Il valore inserito non è valido");
   }
 } while ( n.toString().length != 4 )
 
@@ -16,3 +16,13 @@ for ( var i = 0; i < n.toString().length; i++ ) {
 }
 
 console.log( risutato );
+
+// SOLUZIONE: Siccome le stringhe sono considerate dai programmi come un array di caratteri, ogni carattere è un elemento dell' array stringa.
+
+// var n = prompt("Inserisci un numero di 4 cifre");
+// var somma = 0;
+// for ( var i = 0; i < n.length; i++ ) {
+//   somma += parseInt( n[i] );
+// }
+//
+// console.log( somma );
